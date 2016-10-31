cf1a28c55898043cc1c2013da78ed9edf33f53bd
harshnag <harshnag@users.noreply.github.com>
2016-10-30 18:38:19 -0700

    Add files via upload

--- fantasyfooty/settings.py
+++ fantasyfooty/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
+    'grids.apps.GridsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- grids/models.py
+++ grids/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Rating(models.Model):
+    defense = models.IntegerField(default=0)
+    offense = models.IntegerField(default=0)
+    def __str__(self):
+        return 'Off: %s, Def: %s' % (self.offense, self.defense)
+
+class Player(models.Model):
+    player_name = models.CharField(max_length=200)
+    birth_date = models.DateTimeField('birth date')    
+    rating = models.ForeignKey(Rating, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.player_name
+
+    def was_born_yesterday(self):
+        return self.birth_date >= timezone.now() - datetime.timedelta(days=1)
+        
