c7255602589ac54a3d7aad6c3aad125f994b1dcb
harshnag <harshnag@users.noreply.github.com>
2016-10-30 22:08:16 -0700

    Add files via upload

--- grids/urls.py
+++ grids/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'grids'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<player_id>[0-9]+)/$', views.player, name='player'),
--- grids/views.py
+++ grids/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from django.http import Http404
+from django.template import loader
 
 from .models import Player
 
 def index(request):
     player_list = Player.objects.all()
-    output = ', '.join([p.player_name for p in player_list])
-    return HttpResponse(output)
+    context = {
+        'player_list' : player_list
+        }
+    return render(request, 'grids/index.html', context)
 
 def player(request, player_id):
-    return HttpResponse("You're not my buddy, friend <<%s>>." % player_id)
+    player_list = get_object_or_404(Player, id=player_id)
+    return HttpResponse("Looking up player id: <<%s>> : %s" %
+                        (player_id, player_list.player_name))
