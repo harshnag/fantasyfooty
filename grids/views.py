80102653f81a3fa6a1a5912c779dabd398641788
harshnag <harshnag@users.noreply.github.com>
2016-10-30 18:53:20 -0700

    Add files via upload

--- grids/views.py
+++ grids/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def player(request, player_id):
+    return HttpResponse("You're not my buddy, friend <<%s>>." % player_id)
